eb3a3625d759f7b3bdcd5302cccf44a81bd6c867
Dongha Kim <vataltrick@gmail.com>
2023-03-27 13:03:56 +0900

    .

--- code/zCompute/test_deployment.tf
+++ code/zCompute/test_deployment.tf
@@ -1,20 +1,6 @@
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  endpoints {
-    ec2         = "https://${var.zCompute_ip}/api/v2/aws/ec2"
-    elb         = "https://${var.zCompute_ip}/api/v2/aws/elbv2"
-    s3          = "https://${var.zCompute_ip}:1061/"
-    rds         = "https://${var.zCompute_ip}/api/v2/aws/rds"
-    iam         = "https://${var.zCompute_ip}/api/v2/aws/iam"
-    route53     = "https://${var.zCompute_ip}/api/v2/aws/route53"
-    sts         = "https://${var.zCompute_ip}/api/v2/aws/sts"
-  }
-  region                      = "us-east-2"
-  insecure                    = true
-  skip_metadata_api_check     = true
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
 }
 
 # Creating VPC

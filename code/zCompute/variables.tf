b7559b0b0063454b09a58ca14db3eb3305ae1355
Dongha Kim <vataltrick@gmail.com>
2023-03-27 14:24:49 +0900

    .

---
+++ code/zCompute/variables.tf
@@ -0,0 +1,16 @@
+variable "zCompute_ip" {
+  default = "115.71.6.118"
+}
+
+variable "access_key" {
+  default = "cc6771dc04c645f9ba08a5f1070a8534"
+}
+
+variable "secret_key" {
+  default = "f3dd95749bee4c9f9cc37715865ebb0a"
+}
+
+variable "image_ami" {
+  description = "Ubuntu OS"
+  default     = "ami-80f02613a66a4e58a758f98d182f9489"
+}

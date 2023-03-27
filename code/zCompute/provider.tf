c8022d17efc675096a530d9d11195adaf3c602c2
Dongha Kim <vataltrick@gmail.com>
2023-03-27 13:54:57 +0900

    .

--- code/zCompute/provider.tf
+++ code/zCompute/provider.tf
@@ -14,15 +14,4 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_credentials_validation = true
   skip_requesting_account_id  = true
-  endpoints {
-    ec2        = "https://${var.zcompute_ip}/api/v2/aws/ec2"
-    elb        = "https://${var.zcompute_ip}/api/v2/aws/elbv2"
-    iam        = "https://${var.zcompute_ip}/api/v2/aws/iam"
-    route53    = "https://${var.zcompute_ip}/api/v2/aws/route53"
-    asg        = "https://${var.zcompute_ip}/api/v2/aws/autoscaling"
-    cloudwatch = "https://${var.zcompute_ip}/api/v2/aws/cloudwatch"
-    sns        = "https://${var.zcompute_ip}/api/v2/aws/sns"
-    efs        = "https://${var.zcompute_ip}/api/v2/aws/efs"
-    s3         = "https://${var.zcompute_ip}:1061/"
-  }
 }

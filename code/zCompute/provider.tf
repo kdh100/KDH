6a7c06ed7241b442a348bcac3755706a2d9a1805
Dongha Kim <vataltrick@gmail.com>
2023-03-27 14:00:58 +0900

    .

--- code/zCompute/provider.tf
+++ code/zCompute/provider.tf
@@ -10,8 +10,4 @@ terraform {
 provider "aws" {
   access_key                  = var.access_key
   secret_key                  = var.secret_key
-  insecure                    = true
-  skip_metadata_api_check     = true
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
 }

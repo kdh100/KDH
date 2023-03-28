f6a1210e8db543e8e26e89b8c5e48e94cf364725
Dongha Kim <vataltrick@gmail.com>
2023-03-28 10:36:11 +0900

    2023.03.28 first python code works!

--- code/python/zos_containerlist.py
+++ code/python/zos_containerlist.py
@@ -2,5 +2,5 @@ import boto3
 
 s3 = boto3.resource('s3', endpoint_url="https://vsa-00000004-kt-object-01.zadarazios.com")
 
-for bucket in s3.bucket.all():
+for bucket in s3.buckets.all():
     print(bucket.name)

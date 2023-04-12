be9542945de7e2682efb5045133f7bc45072a13b
Dongha Kim <vataltrick@gmail.com>
2023-04-12 15:19:52 +0900

    .

--- zCompute/instance.tf
+++ zCompute/instance.tf
@@ -21,14 +21,29 @@ resource "aws_internet_gateway" "igw_kdh" {
   vpc_id = aws_vpc.vpc1_kdh.id
 }
 
-# associate igw including default route with vpc
-resource "aws_default_route_table" "rt_kdh" {
-  default_route_table_id = aws_vpc.vpc1_kdh.default_route_table_id
+# create route table for front-end with igw
+resource "aws_route_table" "fe_rt_kdh" {
+  vpc_id = aws_vpc.vpc1_kdh.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw_kdh.id
   }
 }
+
+# associate front-end route table to public subnet
+resource "aws_route_table_association" "fe_rt_kdh_association" {
+  subnet_id = aws_subnet.public_subnet1.id
+  route_table_id = aws_route_table.fe_rt_kdh.id
+}
+
+# # associate igw including default route with vpc
+# resource "aws_default_route_table" "rt_kdh" {
+#   default_route_table_id = aws_vpc.vpc1_kdh.default_route_table_id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.igw_kdh.id
+#   }
+# }
 
 # create dhcp option
 resource "aws_vpc_dhcp_options" "dhcprule1" {
@@ -42,9 +57,9 @@ resource "aws_vpc_dhcp_options_association" "dhcprule_association" {
   dhcp_options_id = aws_vpc_dhcp_options.dhcprule1.id
 }
 
-# create security group
-resource "aws_security_group" "sg1_kdh" {
-  name = var.security_group_name
+# create security group for front-end service
+resource "aws_security_group" "fe_sg_kdh" {
+  name = var.frontend_security_group_name
   vpc_id = aws_vpc.vpc1_kdh.id
   ingress {
     from_port   = 0
@@ -60,17 +75,65 @@ resource "aws_security_group" "sg1_kdh" {
   }
 }
 
-# create instances
-resource "aws_instance" "vm0" {
-  ami                    = var.image_ami
-  instance_type          = var.instance_type
-  subnet_id              = aws_subnet.private_subnet1.id
-  vpc_security_group_ids = [aws_security_group.sg1_kdh.id]
+# create security group for back-end service
+resource "aws_security_group" "be_sg_kdh" {
+  name = var.backend_security_group_name
+  vpc_id = aws_vpc.vpc1_kdh.id
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
+# create instance in public subnet
+# it would be front-end service for
 resource "aws_instance" "vm1" {
   ami                    = var.image_ami
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public_subnet1.id
-  vpc_security_group_ids = [aws_security_group.sg1_kdh.id]
+  vpc_security_group_ids = [aws_security_group.fe_sg_kdh.id]
+}
+
+# create instance in private subnet
+# it would be back-end service for
+resource "aws_instance" "vm0" {
+  ami                    = var.image_ami
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.private_subnet1.id
+  vpc_security_group_ids = [aws_security_group.be_sg_kdh.id]
+}
+
+# create eip for nat
+resource "aws_eip" "eip1_kdh" {
+  vpc = true
+}
+
+# create nat for back-end service
+resource "aws_nat_gateway" "nat_kdh" {
+  allocation_id = aws_eip.eip1_kdh.id
+  subnet_id = aws_subnet.public_subnet1.id
+  depends_on = [aws_internet_gateway.igw_kdh]
+}
+
+# create route table for back-end with nat
+resource "aws_route_table" "be_rt_kdh" {
+  vpc_id = aws_vpc.vpc1_kdh.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_kdh.id
+  }
+}
+
+# associate back-end route table to private subnet
+resource "aws_route_table_association" "be_rt_kdh_association" {
+  subnet_id = aws_subnet.private_subnet1.id
+  route_table_id = aws_route_table.be_rt_kdh.id
 }

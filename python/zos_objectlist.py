38a4a65f9870d4acc711944693850c43647e57a6
Dongha Kim <vataltrick@gmail.com>
2023-04-17 17:29:20 +0900

    2023.04.17 S3SDK list_objects worked code update

--- python/zos_objectlist.py
+++ python/zos_objectlist.py
@@ -2,5 +2,9 @@ import boto3
 
 s3_client = boto3.client('s3', endpoint_url="https://vsa-00000004-kt-object-01.zadarazios.com")
 
-def object_list(bucketname, prefix):
-    
+response = s3_client.list_objects(
+    Bucket=str(input("Type bucketname to browse : "))
+)
+
+for item in response['Contents']:
+    print(f'  {item["Key"]}')
